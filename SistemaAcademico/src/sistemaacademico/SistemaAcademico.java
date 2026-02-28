38684ef0b650ebc607cc0ccebe535270e453e97e
Cesar <caugustorivero@unicesar.edu.co>
2026-02-28 10:27:34 -0500

    feat: inicio de proyecto del sistema academico

---
+++ SistemaAcademico/src/sistemaacademico/SistemaAcademico.java
@@ -0,0 +1,42 @@
+
+package sistemaacademico;
+
+/**
+ *
+ * @author cesar
+ */
+public class SistemaAcademico {
+    // Actividad 1: Definir listas y scanner 
+    static ArrayList<Estudiante> estudiantes = new ArrayList<>();
+    static ArrayList<Asignatura> asignaturas = new ArrayList<>();
+    static ArrayList<Nota> notas = new ArrayList<>();
+    static Scanner leer = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+    public static void mostrarMenu() {
+        int opcion = 0;
+    do {
+        System.out.println("Registro academico");
+        System.out.println("1. Registrar Estudiante");
+        System.out.println("2. mostrar Estudiantes");
+        System.out.println("3. Registrar Asignatura");
+        System.out.println("4. Registrar Nota");
+        System.out.println("5. Salir");
+        System.out.print("Seleccione una opción: ");
+        
+        opcion = leer.nextInt();
+        switch (opcion) {
+            case 1: registrarEstudiante(); break;
+            case 2: listarEstudiantes(); break;
+            case 3: /* Aquí conectaremos al Desarrollador 1 */ break;
+            case 4: /* Aquí conectaremos al Desarrollador 2 */ break;
+            case 5: System.out.println("Saliendo..."); break;
+            default: System.out.println("Opción no válida.");
+        }
+    } while (opcion != 5);
+}
+}
+
+
