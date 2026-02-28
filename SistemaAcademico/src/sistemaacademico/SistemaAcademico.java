e9a7d6621b2d7f9fbc0d58ed603afa94d0f37c49
Cesar <caugustorivero@unicesar.edu.co>
2026-02-28 11:20:20 -0500

    feat: actualizar comentarios

--- SistemaAcademico/src/sistemaacademico/SistemaAcademico.java
+++ SistemaAcademico/src/sistemaacademico/SistemaAcademico.java
@@ -1,10 +1,6 @@
 
 package sistemaacademico;
 
-/**
- *
- * @author cesar
- */
 public class SistemaAcademico {
     static ArrayList<Estudiante> estudiantes = new ArrayList<>();
     static ArrayList<Asignatura> asignaturas = new ArrayList<>();
