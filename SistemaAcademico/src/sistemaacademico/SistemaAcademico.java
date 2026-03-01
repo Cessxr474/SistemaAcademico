6ecac8b5a3447a70cac5efdf998cf595f2dd3e59
Santiago <sbautistav@unicesar.edu.co>
2026-02-28 19:12:26 -0500

    feat: implementar Create - registrar asignatura, implementar READ - Buscar asginatura, implementar DELETE - eliminar asignatura

--- SistemaAcademico/src/sistemaacademico/SistemaAcademico.java
+++ SistemaAcademico/src/sistemaacademico/SistemaAcademico.java
@@ -36,6 +36,55 @@ public class SistemaAcademico {
         }
     } while (opcion != 5);
 }
+    
+    public static void registrarAsignatura(){
+        System.out.println("Ingrese el codigo de la asignatura: ");
+        String codigo= leer.nextLine();
+        System.out.println("Ingrese el nombre de la asignatura: ");
+        String nombre = leer.nextLine();
+        System.out.println("Ingrese la cantidad de creditos de la asignatura: ");
+        int creditos = leer.nextInt();
+        System.out.println("Ingrese el nombre del docente que dicta la asignatura: ");
+        String docente = leer.nextLine();
+        
+      Asignatura asig = new Asignatura(codigo, nombre, creditos, docente);
+      asignaturas.add(asig);
+      
+        System.out.println("Asigantura registrada con exito");
+    }
+    
+    public static void buscarAsignatura(){
+        System.out.println("Ingrese el codigo de la asignatura que quiere buscar: ");
+        String cod = leer.nextLine();
+        
+        for (Asignatura l : asignaturas) {
+            if(l.getCodigo().equals(cod)){
+                System.out.println("La asignatura es: "+l.getNombre()+"Dictada por el docente: "+l.getDocente());
+            } else {
+                System.out.println("No se ha encontrado la asignatura: ");
+            }
+        }
+    }
+    
+    public static void eliminarAsignatura(){
+        boolean eliminado=false;
+        System.out.println("Digite el codigo de la asignatura que desea eliminar: ");
+        String codi= leer.nextLine();
+        
+        for (Asignatura e : asignaturas) {
+            if(e.getCodigo().equals(e)){
+            asignaturas.remove(e);
+            eliminado=true;
+            break;
+            }
+        }
+        
+        if(eliminado==true){
+            System.out.println("La asignatura se ha eliminado correctamente: ");
+        } else{
+            System.out.println("No se ha encontrado el codigo de la asignatura que quiere eliminar");
+        }
+    }
 }
 
-
+    
