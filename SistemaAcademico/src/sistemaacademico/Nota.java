988157a3428dd746135b7ef407dba3538da17d7e
Moises <micarrillo@unicesar.edu.co>
2026-03-03 08:44:34 -0500

    feat: implementar metodos del sistema academico

--- SistemaAcademico/src/sistemaacademico/Nota.java
+++ SistemaAcademico/src/sistemaacademico/Nota.java
@@ -4,10 +4,60 @@
  */
 package sistemaacademico;
 
-/**
- *
- * @author moise
- */
 public class Nota {
-    
+
+    private int id;
+    private String nombreEstudiante;
+    private String asignatura;
+    private double valor;
+
+    public Nota() {
+    }
+
+    public Nota(int id, String nombreEstudiante, String asignatura, double valor) {
+        this.id = id;
+        this.nombreEstudiante = nombreEstudiante;
+        this.asignatura = asignatura;
+        this.valor = valor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreEstudiante() {
+        return nombreEstudiante;
+    }
+
+    public void setNombreEstudiante(String nombreEstudiante) {
+        this.nombreEstudiante = nombreEstudiante;
+    }
+
+    public String getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(String asignatura) {
+        this.asignatura = asignatura;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+               " | Estudiante: " + nombreEstudiante +
+               " | Asignatura: " + asignatura +
+               " | Nota: " + valor;
+    }
 }
